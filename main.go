f07aec357f19e00c598681989eece79b4d701937
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-02-19 04:59:16 +0000

    removed pub/index

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	flag.StringVar(&port, "port", "80", "port number")
 	flag.Parse()
 
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.Handle("/", http.FileServer(http.Dir("/var/www/html/public")))
 	http.ListenAndServe(":"+port, nil)
 }
