a0c5750d973f2ecbf29c4302896971d718cf3c7b
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-04-22 00:35:36 +0000

    adding metrics

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"flag"
 	"net/http"
+	
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	var port string
 	flag.StringVar(&port, "port", "80", "port number")
 	flag.Parse()
-
+    http.Handle("/metrics", promhttp.Handler())
 	http.Handle("/", http.FileServer(http.Dir("/var/www/html/public")))
-	http.ListenAndServe(":"+port, nil)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
